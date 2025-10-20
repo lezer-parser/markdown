26aca393656b7c5fbda34cba8c08a76410bcf24c
Marijn Haverbeke <marijn@haverbeke.berlin>
2025-10-20 12:13:42 +0200

    Properly check for opening flag in findOpeningDelimiter
    FIX: Fix an issue where `findOpeningDelimiter` could return the position of
    a close delimiter.
    
    Closes https://github.com/lezer-parser/markdown/pull/56

--- src/markdown.ts
+++ src/markdown.ts
@@ -1744,7 +1744,7 @@ export class InlineContext {
   findOpeningDelimiter(type: DelimiterType) {
     for (let i = this.parts.length - 1; i >= 0; i--) {
       let part = this.parts[i]
-      if (part instanceof InlineDelimiter && part.type == type) return i
+      if (part instanceof InlineDelimiter && part.type == type && (part.side & Mark.Open)) return i
     }
     return null
   }
